e984212906267dbfc5698da684144547ed150d85
Duncan de Boer <duncan.d.boer@gmail.com>
2026-01-23 08:49:21 +0100

    docs: add examples for openprovider_domain

---
+++ examples/domain/simple.tf
@@ -0,0 +1,5 @@
+resource "openprovider_domain" "example" {
+  name         = "example.com"
+  owner_handle = "owner123"
+  period       = 1
+}
---
+++ examples/resources/openprovider_domain/full.tf
@@ -0,0 +1,9 @@
+resource "openprovider_domain" "prod" {
+  name           = "mydomain.com"
+  owner_handle   = "owner123"
+  admin_handle   = "admin456"
+  tech_handle    = "tech789"
+  billing_handle = "bill001"
+  period         = 2
+  autorenew      = true
+}
